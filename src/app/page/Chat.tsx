d177045aa6f943dc4b7348fd131f329b3e683134
Rony Mateo <ronymateo@gmail.com>
2020-06-28 21:07:26 -0500

    feat(font) add lato from google fonts and awesomefonts

--- src/app/page/Chat.tsx
+++ src/app/page/Chat.tsx
@@ -1,5 +1,48 @@
 import React from 'react'
 
-export default function Chat(){
-    return (<div>Chat</div>)
+export default function Chat() {
+    return (
+        <div className="chat">
+            <section className="messages">
+                <div className="left">
+                    <div className="user">
+                        <p>guest001, 10:02</p>
+                    </div>
+                    <div className="content">
+                        <span> whant to bag to nigth</span>
+                    </div>
+                </div>
+                <div className="left">
+                    <div className="user">
+                        <p>guest001, 10:02</p>
+                    </div>
+                    <div className="content">
+                        <span>I meant hang</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="user">
+                        <p>guest001, 10:02</p>
+                    </div>
+                    <div className="content">
+                        <span>I meant hang</span>
+                    </div>
+                </div>
+                <div className="left">
+                    <div className="user">
+                        <p>guest001, 10:02</p>
+                    </div>
+                    <div className="content">
+                        <span>I meant hang</span>
+                    </div>
+                </div>
+            </section>
+            <form id="chat">
+                <input type="text"></input>
+                <button className="button">
+                    <i className="fas fa-paper-plane fa-2x"></i>
+                </button>
+            </form>
+        </div>
+    )
 }
