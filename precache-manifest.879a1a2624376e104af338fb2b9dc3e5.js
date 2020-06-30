691a6427f4418fed0282dfd0d635075f354535ef
Rony Mateo <ronymateo@gmail.com>
2020-06-30 14:22:42 -0500

    Updates

---
+++ precache-manifest.879a1a2624376e104af338fb2b9dc3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5ec1feefb2f7ae3697c69bf975f663",
+    "url": "/doclerholdchat/index.html"
+  },
+  {
+    "revision": "96108da6ecd3f662f486",
+    "url": "/doclerholdchat/static/css/main.3ae883ae.chunk.css"
+  },
+  {
+    "revision": "5ba3f82fed042785e642",
+    "url": "/doclerholdchat/static/js/2.1060044c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/doclerholdchat/static/js/2.1060044c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96108da6ecd3f662f486",
+    "url": "/doclerholdchat/static/js/main.ea817590.chunk.js"
+  },
+  {
+    "revision": "c91317b51eb2761a9a0e",
+    "url": "/doclerholdchat/static/js/runtime-main.de8c3eba.js"
+  }
+]);
